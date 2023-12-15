701ae6709224ba6626ea8df8444f0cdb19abe6ca
sarulrajan <sarulrajan@gmail.com>
2023-12-15 12:17:32 +0530

    python dictionary program

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+''' to check dictionary key present or not"
+
+
+d={'A':1,'B':2,'C':3}
+key=raw_input("Enter key to check:")
+if key in d.keys():
+      print("Key is present and value of the key is:")
+      print(d[key])
+else:
+      print("Key isn't present!")
